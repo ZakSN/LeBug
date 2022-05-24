379eee34a3c99c1d6dea3ca10466f73c3df643cd
Zakary Nafziger <worldofzak@gmail.com>
2022-05-24 15:33:51 -0700

    add compression probability sweep

--- examples/emu_test/compression_test.py
+++ examples/emu_test/compression_test.py
@@ -97,7 +97,7 @@ class TestCompression(unittest.TestCase):
         # ideal compression ratio is DELTA_SLOTS:1
         return decompressed.shape[0]/full_addrs
 
-    def basic_functionality(self,N,TB_SIZE,DATA_WIDTH,DELTA_SLOTS,frame_length):
+    def basic_functionality(self,N,TB_SIZE,DATA_WIDTH,DELTA_SLOTS,frame_length,p):
         PRECISION = int(DATA_WIDTH/DELTA_SLOTS)
         INV = twos_complement_min(PRECISION)
 
@@ -109,7 +109,7 @@ class TestCompression(unittest.TestCase):
         dd = DeltaDecompressor(N,DATA_WIDTH,DELTA_SLOTS,TB_SIZE)
 
         # create an input frame
-        frame_in = self.build_frame(frame_length, N, DATA_WIDTH, PRECISION, p_compress=0.9)
+        frame_in = self.build_frame(frame_length, N, DATA_WIDTH, PRECISION, p_compress=p)
 
         # feed the frame into the dc->tb emulator:
         for i in range(frame_in.shape[0]):
@@ -144,9 +144,14 @@ class TestCompression(unittest.TestCase):
         '''
         dummy test for debugging specific parameter configurations
         '''
-        self.basic_functionality(1,4,8,4,256)
+        self.basic_functionality(1,4,8,4,256,0.9)
 
     def test_paramteric_sweep(self):
+        '''
+        This test is huge, and fairly slow. Conceptually it ought to easy to
+        parallelize, but it seemsl ike the unit test framework wants to share
+        state between subtests, which makes it annoying in practice.
+        '''
         # set parameters for sweep
         N_param = [1, 2, 4, 8, 16, 32]
         TB_SIZE_param = [4, 8, 16, 32, 64, 128, 256, 512]
@@ -156,19 +161,22 @@ class TestCompression(unittest.TestCase):
         DATA_WIDTH_param = [8, 16, 32,]# 64]
         DELTA_SLOTS_param = [2, 4, 8]
         frame_length_param = [4, 16, 64, 256, 1024]
+        compression_prob = [0.0, 0.2, 0.5, 0.9, 1]
 
         for N in N_param:
             for TB_SIZE in TB_SIZE_param:
                 for DATA_WIDTH in DATA_WIDTH_param:
                     for DELTA_SLOTS in DELTA_SLOTS_param:
                         for frame_length in frame_length_param:
-                            msg = "N="+str(N)+"\n"\
-                                 +"TB_SIZE="+str(TB_SIZE)+"\n"\
-                                 +"DATA_WIDTH="+str(DATA_WIDTH)+"\n"\
-                                 +"DELTA_SLOTS="+str(DELTA_SLOTS)+"\n"\
-                                 +"frame_length="+str(frame_length)+"\n"
-                            with self.subTest(msg=msg):
-                                self.basic_functionality(N,TB_SIZE,DATA_WIDTH,DELTA_SLOTS,frame_length)
+                            for p in compression_prob:
+                                msg = "N="+str(N)+"\n"\
+                                     +"TB_SIZE="+str(TB_SIZE)+"\n"\
+                                     +"DATA_WIDTH="+str(DATA_WIDTH)+"\n"\
+                                     +"DELTA_SLOTS="+str(DELTA_SLOTS)+"\n"\
+                                     +"frame_length="+str(frame_length)+"\n"\
+                                     +"p="+str(p)+"\n"
+                                with self.subTest(msg=msg):
+                                    self.basic_functionality(N,TB_SIZE,DATA_WIDTH,DELTA_SLOTS,frame_length,p)
 
 
 if __name__=="__main__":
