e9e9f0b2bed30bf9b6fa6607b7c0b2e53f3fe249
Zakary Nafziger <worldofzak@gmail.com>
2022-05-31 18:23:13 -0700

    first pass at updating the tracebuffer
    haven't sim'd/synth'd yet

--- src/hardware/buildingBlocks/general/trace_buffer.sv
+++ src/hardware/buildingBlocks/general/trace_buffer.sv
@@ -13,9 +13,12 @@
   input logic clk,
   input logic tracing,
   input logic valid_in,
-  input logic [$clog2(TB_SIZE)-1:0] tb_mem_address,
+  input logic compression_flag_in,
+  input logic inc_tb_ptr,
+  input logic [$clog2(TB_SIZE)-1:0] tb_read_address,
   input logic [DATA_WIDTH-1:0] vector_in [N-1:0],
-  output reg [DATA_WIDTH-1:0] vector_out [N-1:0]
+  output reg [DATA_WIDTH-1:0] vector_out [N-1:0],
+  output reg compression_flag_out
  );
 
     //----------Internal Variables------------
@@ -27,53 +30,82 @@
 
     //-------------Code Start-----------------
 
-    // Instantiate memory to implement queue
-    reg [$clog2(TB_SIZE)-1:0] mem_address_a=0;
-    wire mem_write_enable_a;
-    reg mem_write_enable_b=0;
-    wire [MEM_WIDTH-1:0] mem_in_a;
-    reg [MEM_WIDTH-1:0] mem_in_b=0;
-    wire [MEM_WIDTH-1:0] mem_out_a;
-    wire [MEM_WIDTH-1:0] mem_out_b;
-    ram_dual_port mem (
+    // Instantiate memory to implement circular trace buffer
+    reg [$clog2(TB_SIZE)-1:0] tb_ptr=0;
+    wire write_enable_a;
+    reg write_enable_b=0;
+    wire [MEM_WIDTH-1:0] tbuffer_in_a;
+    reg [MEM_WIDTH-1:0] tbuffer_in_b=0;
+    wire [MEM_WIDTH-1:0] tbuffer_out_a;
+    wire [MEM_WIDTH-1:0] tbuffer_out_b;
+    ram_dual_port tbuffer (
       .clk( clk ),
       .clken( 1'b1 ),
-      .address_a( mem_address_a ),
-      .address_b( tb_mem_address ),
-      .wren_a( mem_write_enable_a ),
-      .wren_b( mem_write_enable_b ),
-      .data_a( mem_in_a ),
-      .data_b( mem_in_b ),
+      .address_a( tb_ptr ),
+      .address_b( tb_read_address ),
+      .wren_a( write_enable_a ),
+      .wren_b( write_enable_b ),
+      .data_a( tbuffer_in_a ),
+      .data_b( tbuffer_in_b ),
       .byteena_a( 1'b1 ),
       .byteena_b( 1'b1 ),
-      .q_a( mem_out_a ),
-      .q_b( mem_out_b)
+      .q_a( tbuffer_out_a ),
+      .q_b( tbuffer_out_b)
     );
-    defparam mem.width_a = MEM_WIDTH;
-    defparam mem.width_b = MEM_WIDTH;
-    defparam mem.widthad_a = $clog2(TB_SIZE);
-    defparam mem.widthad_b = $clog2(TB_SIZE);
-    defparam mem.width_be_a = 1;
-    defparam mem.width_be_b = 1;
-    defparam mem.numwords_a = TB_SIZE;
-    defparam mem.numwords_b = TB_SIZE;
-    defparam mem.latency = RAM_LATENCY;
-    defparam mem.init_file = "traceBuffer.mif";
+    defparam tbuffer.width_a = MEM_WIDTH;
+    defparam tbuffer.width_b = MEM_WIDTH;
+    defparam tbuffer.widthad_a = $clog2(TB_SIZE);
+    defparam tbuffer.widthad_b = $clog2(TB_SIZE);
+    defparam tbuffer.width_be_a = 1;
+    defparam tbuffer.width_be_b = 1;
+    defparam tbuffer.numwords_a = TB_SIZE;
+    defparam tbuffer.numwords_b = TB_SIZE;
+    defparam tbuffer.latency = RAM_LATENCY;
+    defparam tbuffer.init_file = "traceBuffer.mif";
+
+    // Instantiate memory to implement circular compression flag buffer
+    reg cfbuffer_in_b=0;
+    wire cfbuffer_out_a;
+    ram_dual_port cfbuffer (
+      .clk( clk ),
+      .clken( 1'b1 ),
+      .address_a( tb_ptr ),
+      .address_b( tb_read_address ),
+      .wren_a( write_enable_a ),
+      .wren_b( write_enable_b ),
+      .data_a( compression_flag_in ),
+      .data_b( cfbuffer_in_b ),
+      .byteena_a( 1'b1 ),
+      .byteena_b( 1'b1 ),
+      .q_a( cfbuffer_out_a ),
+      .q_b( compression_flag_out )
+    );
+    defparam cfbuffer.width_a = 1;
+    defparam cfbuffer.width_b = 1;
+    defparam cfbuffer.widthad_a = $clog2(TB_SIZE);
+    defparam cfbuffer.widthad_b = $clog2(TB_SIZE);
+    defparam cfbuffer.width_be_a = 1;
+    defparam cfbuffer.width_be_b = 1;
+    defparam cfbuffer.numwords_a = TB_SIZE;
+    defparam cfbuffer.numwords_b = TB_SIZE;
+    defparam cfbuffer.latency = RAM_LATENCY;
+    //defparam cfbuffer.init_file = "compressionFlagBuffer.mif";
 
     always @(posedge clk) begin
 
         // Logic for enqueuing values
-        if (tracing==1'b1 & valid_in==1'b1) begin
-            mem_address_a <= mem_address_a<TB_SIZE-1 ? mem_address_a+1'b1 : 0;
+        if (tracing==1'b1 & valid_in==1'b1 & inc_tb_ptr==1'b1) begin
+            tb_ptr <= tb_ptr<TB_SIZE-1 ? tb_ptr+1'b1 : 0;
         end
     end
 
 
     // Directly assign module inputs to port A of memory
-    assign mem_in_a = { >> { vector_in }};
-    assign mem_write_enable_a = valid_in;
+    assign tbuffer_in_a = { >> { vector_in }};
+    assign tbuffer_write_enable_a = valid_in;
+    assign cfbuffer_write_enable_a = valid_in;
 
     // Module output comes from port b (need to drive it when dumping the content)
-    assign vector_out = { >> { mem_out_b }};
+    assign vector_out = { >> { tbuffer_out_b }};
  
  endmodule 
