1cfcadb2c521643cac28f102cf7c19b1b5b60ff0
Zakary Nafziger <worldofzak@gmail.com>
2022-05-31 17:18:00 -0700

    working delta_compressor
    - I hope

--- src/hardware/buildingBlocks/general/delta_compressor.sv
+++ src/hardware/buildingBlocks/general/delta_compressor.sv
@@ -2,62 +2,6 @@
  // Design Name : Delta Compressor
  // Function    : Performs delta compression on N vectors before sending them to the tracebuffer
  //-----------------------------------------------------
-
-module compressionRegisterAtom #(
- parameter PRECISION,
- parameter INV,
- parameter ID
- )
- (
-  input integer ptr,
-  input logic [PRECISION-1:0] i,
-  input logic [PRECISION-1:0] delta,
-  output reg [PRECISION-1:0] o
- );
-
- always_comb begin
-    if (ptr < ID) begin
-      o <= INV;
-    end
-    else if (ptr == ID) begin
-      o <= delta;
-    end
-    else begin
-      o <= i;
-    end
- end
-endmodule
-
-module compressionRegisterSlice #(
- parameter DELTA_SLOTS,
- parameter PRECISION,
- parameter INV
- )
- (
-  input integer ptr,
-  input logic [(DELTA_SLOTS*PRECISION)-1:0] I,
-  input logic [(DELTA_SLOTS*PRECISION)-1:0] delta,
-  output reg [(DELTA_SLOTS*PRECISION)-1:0] O
- );
-
-  genvar atomID;
-  generate
-    for ( atomID = 0; atomID < DELTA_SLOTS; atomID++) begin: atomIDgen
-      compressionRegisterAtom #(
-        .PRECISION(PRECISION),
-        .INV(INV),
-        .ID(DELTA_SLOTS - 1 - atomID)
-      )
-      cra (
-        .ptr(ptr),
-        .i(I[(atomID+1)*PRECISION-1:atomID*PRECISION]),
-        .delta(delta[PRECISION-1:0]),
-        .o(O[(atomID+1)*PRECISION-1:atomID*PRECISION])
-      );
-    end
-  endgenerate
-endmodule
-
 module deltaCompressor #(
  parameter N=8,
  parameter DATA_WIDTH=32,
@@ -89,39 +33,26 @@ module deltaCompressor #(
 
   reg [DATA_WIDTH-1:0] last_vector [N-1:0];
   reg [DATA_WIDTH-1:0] delta [N-1:0];
-  reg [DATA_WIDTH-1:0] comp_reg_D [N-1:0];
-  reg [DATA_WIDTH-1:0] comp_reg_Q [N-1:0];
+  reg [DATA_WIDTH-1:0] comp_reg [N-1:0];
+  reg [DATA_WIDTH-1:0] cr_upper_mask;
+  reg [DATA_WIDTH-1:0] cr_lower_mask;
   reg first_cycle;
   reg [N-1:0] overflow;
 
   integer i;
+  integer j;
   integer ptr;
-
-  genvar CrSlice;
-  generate
-    for ( CrSlice = 0; CrSlice < N; CrSlice++) begin: CrSliceGen
-      compressionRegisterSlice #(
-       .DELTA_SLOTS(DELTA_SLOTS),
-       .PRECISION(PRECISION),
-       .INV(INV)
-       )
-       crs (
-        .ptr(ptr),
-        .I(comp_reg_Q[CrSlice]),
-        .delta(delta[CrSlice]),
-        .O(comp_reg_D[CrSlice])
-       );
-    end
-  endgenerate
 
   always @(posedge clk) begin
     if (valid_in == 1'b1 && tracing==1'b0) begin
-      comp_reg_Q = comp_reg_D;
       // starting condition; last_vector is invalid and must be re-initialized
       if (first_cycle == 1'b1) begin
         last_vector = vector_in;
         valid_out = 1'b0;
         first_cycle = 1'b0;
+        for (i = 0; i < N; i++) begin
+          comp_reg[i] = NODATA;
+        end
       end
       else begin
         // we've seen at least one vector; start compressing
@@ -137,10 +68,25 @@ module deltaCompressor #(
         end
 
         if (|overflow != 1'b1) begin // no overflow
+          // update the compression register
+          cr_upper_mask = {DATA_WIDTH{1'b1}};
+          cr_lower_mask = {DATA_WIDTH{1'b1}};
+          for (j = 0; j < DELTA_SLOTS; j++) begin
+            if (j < (DELTA_SLOTS-ptr)) begin
+              cr_upper_mask = cr_upper_mask << PRECISION;
+            end
+            if (j <= ptr) begin
+              cr_lower_mask = cr_lower_mask >> PRECISION;
+            end
+          end
+          for (i = 0; i < N; i++) begin
+            comp_reg[i] = (comp_reg[i]&cr_upper_mask) + ((delta[i]&{PRECISION{1'b1}}) << (DELTA_SLOTS-ptr-1)*(PRECISION)) + (NODATA&cr_lower_mask);
+          end
+
           // set output signals
           valid_out = 1'b1;
           v_out_comp = COMPRESSED;
-          vector_out = comp_reg_D;
+          vector_out = comp_reg;
           if (ptr==0) begin
             inc_tb_ptr = 1'b1;
           end
@@ -161,7 +107,10 @@ module deltaCompressor #(
           v_out_comp = ~COMPRESSED;
           vector_out = last_vector;
           inc_tb_ptr = 1'b1;
-          ptr = -1;
+          ptr = 0;
+          for (i = 0; i < N; i++) begin
+            comp_reg[i] = NODATA;
+          end
         end
         last_vector = vector_in;
       end
@@ -172,21 +121,7 @@ module deltaCompressor #(
       first_cycle = 1'b1;
       ptr = -1;
       valid_out = 1'b0;
-      for (i = 0; i < N; i++) begin
-        comp_reg_Q[i] = {DELTA_SLOTS{INV}};
-      end
     end
   end
-
-  always @(negedge clk) begin
-    // XXX: simulation only
-    if (inc_tb_ptr == 1'b1) begin
-      for (i = 0; i < N; i++) begin
-        $write(vector_out[(N-1) - i]);
-        $write(", ");
-      end
-      $display("\n");
-    end
- end
 
 endmodule
