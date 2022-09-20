fix compression formula

examples/emu_test/compression_test.py
@@ -43,12 +43,13 @@ class TestCompressionEmu(unittest.TestCase, TestUtils):
         self.assertTrue(self.check_frame_equality(frame_in, frame_out))
 
         v_nodata = np.full((1, N), n_bit_nodata(DELTA_SLOTS, PRECISION, INV))
-        cr = self.compression_ratio(v_nodata, tbuffer, frame_out)
+        cr = self.compression_ratio(v_nodata, tbuffer, frame_out, N*DATA_WIDTH)
         print(cr)
 
         # ensure that the compressed data did not take more space than the
-        # decompressed data
-        self.assertTrue(cr >= 1)
+        # worst case compression ratio
+        wccr = self.worst_case_cr(v_nodata, tbuffer, N*DATA_WIDTH)
+        self.assertTrue(cr >= wccr)
         # ensure that the reported compression ratio does not exceed the theoretical
         # maximum
         self.assertTrue(cr <= DELTA_SLOTS)

examples/hw_test/compression_test.py
@@ -75,14 +75,16 @@ class TestCompressionHW(unittest.TestCase, TestUtils):
          self.assertTrue(self.check_frame_equality(frame_in, hw_results_decompressed))
 
          # ensure that compressed data took less space than the decompressed data
+         # (accounting for extra CF flag bits in the worst case)
          # we've converted all of the numbers from the tb to signed values, including
          # the nodata symbol (which is properly a bit vector and not a number...)
          # so we need to do some extra math here
          unsigned_nodata = n_bit_nodata(DELTA_SLOTS, PRECISION, INV)
          signed_nodata = unsigned_nodata - 2**(DATA_WIDTH)
          v_nodata = np.full((1, N), signed_nodata)
-         cr = self.compression_ratio(v_nodata, hw_results_compressed[1], hw_results_decompressed)
-         self.assertTrue(cr >= 1)
+         cr = self.compression_ratio(v_nodata, hw_results_compressed[1], hw_results_decompressed, N*DATA_WIDTH)
+         wccr = self.worst_case_cr(v_nodata, hw_results_compressed[1], N*DATA_WIDTH)
+         self.assertTrue(cr >= wccr)
 
          # ensure that the compression ratio does not exceed the theoretical max
          self.assertTrue(cr <= DELTA_SLOTS)

src/experiments/compression_experiment.py
@@ -215,7 +215,8 @@ class CompressionExperiment():
         tuobj = TestUtils()
         nodata = n_bit_nodata(self.emu_cfg['DELTA_SLOTS'], self.emu_cfg['PRECISION'], self.emu_cfg['INV'])
         v_nodata = np.full((1, self.emu_cfg['N']), nodata)
-        cr = tuobj.compression_ratio(v_nodata, log['tb'][-1][0], decomp_tb)
+        bpv = self.emu_cfg['N']*self.emu_cfg['DATA_WIDTH']
+        cr = tuobj.compression_ratio(v_nodata, log['tb'][-1][0], decomp_tb, bpv)
 
         # compute input/output bit 1 probabability for entropy measurement
         # for the output we also need to consider any bits left in the last_reg